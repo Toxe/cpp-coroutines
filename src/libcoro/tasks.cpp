55e63c28453d8c6f4dc5bb0609b2d0e82f3a20b5
Toxe <toxe@toxe.net>
2024-08-14 23:10:58 +0200

    Add libcoro tasks example

---
+++ src/libcoro/tasks.cpp
@@ -0,0 +1,81 @@
+#include <cassert>
+#include <iostream>
+#include <string_view>
+#include <utility>
+
+#define ANKERL_NANOBENCH_IMPLEMENT
+#include "nanobench.h"
+
+#include "coro/coro.hpp"
+
+void print_and_assert_result(const std::string_view label, const uint64_t expected, const uint64_t result)
+{
+    assert(result == expected);
+    std::cout << label << ": " << result << '\n';
+}
+
+coro::task<uint64_t> fibonacci(const int nth)
+{
+    uint64_t a = 0;
+    uint64_t b = 1;
+
+    for (int i = 0; i < nth; ++i)
+        a = std::exchange(b, a + b);
+
+    co_return a;
+}
+
+void run_fibonacci()
+{
+    // create new tasks but don't start executing the coroutines yet
+    const coro::task<uint64_t> fibonacci10_task = fibonacci(10);
+    const coro::task<uint64_t> fibonacci20_task = fibonacci(20);
+
+    // execute the coroutines
+    print_and_assert_result("fibonacci(10)", 55, coro::sync_wait(fibonacci10_task));
+    print_and_assert_result("fibonacci(20)", 6765, coro::sync_wait(fibonacci20_task));
+}
+
+void run_simple_tasks()
+{
+    // create new tasks but don't start executing the coroutines yet
+    const auto make_double_task = [](const int n) -> coro::task<int> { co_return n * 2; };
+    const auto double100_task = make_double_task(100);
+
+    const auto make_double100_and_add_task = [&](const int n) -> coro::task<int> { co_return co_await double100_task + n; };
+    const auto double100_and_add_five_task = make_double100_and_add_task(5);
+
+    // execute the coroutines
+    print_and_assert_result("double100_task", 200, coro::sync_wait(double100_task));
+    print_and_assert_result("double100_and_add_five_task", 205, coro::sync_wait(double100_and_add_five_task));
+}
+
+void benchmark()
+{
+    const auto double100_task = [](const int n) -> coro::task<int> { co_return n * 2; }(100);
+    const auto double100_and_add_five_task = [&](const int n) -> coro::task<int> { co_return co_await double100_task + n; }(5);
+
+    ankerl::nanobench::Bench().run("libcoro: simple_tasks", [&] {
+        const auto a = coro::sync_wait(double100_task);
+        const auto b = coro::sync_wait(double100_and_add_five_task);
+        const auto result = a + b;
+        ankerl::nanobench::doNotOptimizeAway(result);
+        assert(result == 405);
+    });
+
+    const coro::task<uint64_t> fibonacci_task = fibonacci(20);
+
+    ankerl::nanobench::Bench().run("libcoro: fibonacci", [&] {
+        const auto result = coro::sync_wait(fibonacci_task);
+        ankerl::nanobench::doNotOptimizeAway(result);
+        assert(result == 6765);
+    });
+}
+
+int main()
+{
+    run_simple_tasks();
+    run_fibonacci();
+
+    benchmark();
+}
