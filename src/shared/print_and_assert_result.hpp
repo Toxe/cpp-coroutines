19a6f023d52d299a1c8ad2c9dcead4fa3072e5d2
Toxe <toxe@toxe.net>
2024-08-22 08:31:17 +0200

    Prevent warning

--- src/shared/print_and_assert_result.hpp
+++ src/shared/print_and_assert_result.hpp
@@ -5,7 +5,11 @@
 #include <string_view>
 
 template <typename T>
+#if NDEBUG
+void print_and_assert_result(const std::string_view label, const T, const T result)
+#else
 void print_and_assert_result(const std::string_view label, const T expected, const T result)
+#endif
 {
     assert(result == expected);
     std::cout << label << ": " << result << '\n';
